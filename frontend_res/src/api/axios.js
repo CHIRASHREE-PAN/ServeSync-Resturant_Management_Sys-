78d4a63e97b2c2f321d415b0a9fd8d8c4a948b38
Debasmita-pal2004 <debasmitapal18@gmail.com>
2026-08-11 22:32:34 +0530

    axioschange_url

--- frontend_res/src/api/axios.js
+++ frontend_res/src/api/axios.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL:
-    import.meta.env.VITE_API_BASE_URL ||
-    'http://127.0.0.1:8007',
-
+  import.meta.env.VITE_API_BASE_URL,
   timeout: 30000,
 
   headers: {
