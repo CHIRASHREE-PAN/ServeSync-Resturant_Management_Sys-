f2d6a9ca79bfbf2e1f5972d239f8a89c093bfefd
Debasmita-pal2004 <debasmitapal18@gmail.com>
2026-08-12 11:29:22 +0530

    corserrorfixed

--- backend/app/main.py
+++ backend/app/main.py
@@ -62,6 +62,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://serve-sync-resturant-management-sys.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
